d4407b9ae121a770d237ffe74ba3ad2c4e613550
hisasann <hisasann25@gmail.com>
2018-10-03 17:06:29 +0900

    first commit

---
+++ pages/color.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <div>
+      <span>color</span>
+      <span>|</span>
+      <span><nuxt-link to="/schema">scheme</nuxt-link></span>
+    </div>
+
+    <div><img :src="data.image.named" /></div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const hex = Math.floor(Math.random() * 16777215).toString(16)
+    const { data } = await axios.get(`http://www.thecolorapi.com/id?hex=${hex}`)
+    return { data }
+  }
+}
+</script>
