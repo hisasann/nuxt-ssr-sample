9c9a462d7f3c7bdbc97c18fb976cee07aeddd975
hisasann <hisasann25@gmail.com>
2018-10-17 05:13:04 +0900

    Add pages/custom-head.vue

---
+++ pages/custom-head.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="container">
+    <h1>custom-head</h1>
+    <div>
+      <p>path: {{path}}</p>
+      <p>params: {{params}}</p>
+      <p>query: {{query}}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+  // http://localhost:3000/custom-head
+  export default {
+    async asyncData({ route }) {
+      console.log(route);
+      return {
+        path: route.path,
+        params: route.params,
+        query: route.query
+      };
+    },
+
+    data () {
+      return {
+        title: 'Hello World!'
+      }
+    },
+
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          { hid: 'description', name: 'description', content: 'My custom description' }
+        ]
+      }
+    }
+  }
+</script>
