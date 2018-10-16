d96ef7e69e4d2f908d3168a19762f77784ee439e
hisasann <hisasann25@gmail.com>
2018-10-16 17:27:33 +0900

    Add sample url in pages/parent-child.vue

--- pages/parent-child.vue
+++ pages/parent-child.vue
@@ -12,7 +12,7 @@
 <script>
   export default {
     async asyncData({ route }) {
-      // http://localhost:3000/search/foo?a=b
+      // http://localhost:3000/c-01/e-02/
       console.log(route);
       return {
         path: route.path,
